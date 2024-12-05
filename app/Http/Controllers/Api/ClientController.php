c9c22e2f97e55fe22dd2940ae136ea525834cac4
Bruno <brunocostasilva62@gmail.com>
2024-12-05 12:16:41 -0300

    corrigindo salvar led que sumiu

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
     //
+    public function store(Request $request): JsonResponse
+    {
+        $client = Client::create([
+            'email' => $request->get('email'),
+            'fone' => $request->get('fonet'),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'client' => $client,
+            'message' => 'Led capturada com sucesso',
+        ]);
+    }
 }
