d901ad6b99b7fdde8b8ee7ed548526816fb6e278
Bruno <brunocostasilva62@gmail.com>
2024-12-12 15:25:42 -0300

    atualizao bd email nao obrigatorio

--- database/migrations/2024_12_03_210752_create_client_table.php
+++ database/migrations/2024_12_03_210752_create_client_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('client', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->string('fone');
             $table->enum('type_client',['LEAD', 'CLIENTE'])->default('LEAD');
 
